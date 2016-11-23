837f3ef07c23f498a6b13afc264cbe62c236fc16
Luis Fernando Ramírez Salazar <Luis Fernando Ramrez Salazar>
2016-11-22 19:55:19 -0700

    solucion registro

--- app/Mail/ConfirmationEmail.php
+++ app/Mail/ConfirmationEmail.php
@@ -19,35 +19,8 @@ class ConfirmationEmail extends Mailable
 
     public function __construct(User $user)
     {
-      $categorias= Categorias::all();
-        $marca= Marcas::all();
-        $marcas1 =DB::table('marcas')
-           ->where('categoria', '=', 2)
-           ->limit('6')
-           ->get();
-        $marcas2 =DB::table('marcas')
-           ->where('categoria', '=', 2)
-           ->orderBy('id', 'desc')
-           ->limit('5')
-           ->get();
-        $celulares1 =DB::table('marcas')
-           ->where('categoria', '=', 1)
-           ->limit('6')
-           ->get();
-        $celulares2 =DB::table('marcas')
-           ->where('categoria', '=', 1)
-           ->orderBy('id', 'desc')
-           ->limit('2')
-           ->get();
-        $electronica =DB::table('marcas')
-           ->where('categoria', '=', 4)
-           ->limit('4')
-           ->get();
-        $consola =DB::table('marcas')
-           ->where('categoria', '=', 3)
-           ->limit('4')
-           ->get();
-        return $this->user = $user, compact('categorias', 'marca', 'marcas1', 'marcas2', 'celulares1', 'celulares2', 'electronica', 'consola');
+      
+        return $this->user = $user;
     }
 
    
